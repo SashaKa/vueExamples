72bc54309a77c3eea2e293c00650f9551559df54
SashaKa <katushkovasa@gmail.com>
2017-12-20 13:09:50 +0300

    chenges

--- petshop/index.js
+++ petshop/index.js
@@ -1,158 +1,139 @@
 new Vue({
   el: '#app',
-  data: {
-    goods: [
-      {
-        artnumber: '123456',
-        name: 'Корм 1',
-        brand: 'Felix',
-        weight: '1500',
-        quantity: '20',
-        price: '1500',
-        stock: '1'
-      },
-      {
-        artnumber: '123457',
-        name: 'Корм 2',
-        brand: 'Felix',
-        weight: '1100',
-        quantity: '10',
-        price: '1400',
-        stock: '0'
-      },
-      {
-        artnumber: '123458',
-        name: 'Корм 3',
-        brand: 'Felix',
-        weight: '1500',
-        quantity: '20',
-        price: '3500',
-        stock: '1'
-      },
-      {
-        artnumber: '123459',
-        name: 'Корм 4',
-        brand: 'Felix',
-        weight: '1500',
-        quantity: '30',
-        price: '3450',
-        stock: '0'
-      },
-      {
-        artnumber: '1234510',
-        name: 'Корм 5',
-        brand: 'Sheba',
-        weight: '1500',
-        quantity: '20',
-        price: '20',
-        stock: '0'
-      },
-      {
-        artnumber: '1234511',
-        name: 'Корм 6',
-        brand: 'Sheba',
-        weight: '1500',
-        quantity: '60',
-        price: '50',
-        stock: '1'
-      },
-      {
-        artnumber: '1234512',
-        name: 'Корм 7',
-        brand: 'Royal Canin',
-        weight: '150',
-        quantity: '10',
-        price: '150',
-        stock: '1'
-      },
-      {
-        artnumber: '1234512',
-        name: 'Корм 8',
-        brand: 'Royal Canin',
-        weight: '9000',
-        quantity: '1',
-        price: '6000',
-        stock: '0'
-      },
-      {
-        artnumber: '1234513',
-        name: 'Корм 9',
-        brand: 'Whiskas',
-        weight: '800',
-        quantity: '1',
-        price: '10',
-        stock: '1'
-      },
-      {
-        artnumber: '1234515',
-        name: 'Корм 10',
-        brand: 'Whiskas',
-        weight: '1500',
-        quantity: '1',
-        price: '860',
-        stock: '0'
-      },
-      {
-        artnumber: '1234514',
-        name: 'Корм 11',
-        brand: 'Petcurean',
-        weight: '800',
-        quantity: '20',
-        price: '1150',
-        stock: '1'
-      },
-      {
-        artnumber: '123456654',
-        name: 'Корм 12',
-        brand: 'AlmoNature',
-        weight: '500',
-        quantity: '10',
-        price: '1800',
-        stock: '1'
-      }
-    ],
-    sorted: [],
-    allBrands: [],
-    checkedBrands:[],
-    currentPage: 1,
-    totalPage: 5,
-    pageSize: 4,
-    showFromTo: 0,
-    showUpTo: 4
-  },
-  beforeDisplay() {
-    const obj = {}
-    for (i = 0; i < this.goods.length; i++) {
-      const str = this.goods[i].brand
-      obj[str] = true
-    }
-    // this.goods= goods
-    // this.sorted = goods
-    // this.allBrands = Object.keys(obj)
-    console.log(this.goods)
-  },
-  computed: {
-    orderedList() {
-      const list = this.sorted.slice(this.showFromto, this.showUpto)
-      this.totalPage = Math.ceil(this.sorted.length / this.pageSize)
-      return list
+  data() {
+    return {
+      goods: [
+        {
+          artnumber: '123456',
+          name: 'Корм 1',
+          brand: 'Felix',
+          weight: '1500',
+          quantity: '20',
+          price: '1500',
+          stock: '1'
+        },
+        {
+          artnumber: '123457',
+          name: 'Корм 2',
+          brand: 'Felix',
+          weight: '1100',
+          quantity: '10',
+          price: '1400',
+          stock: '0'
+        },
+        {
+          artnumber: '123458',
+          name: 'Корм 3',
+          brand: 'Felix',
+          weight: '1500',
+          quantity: '20',
+          price: '3500',
+          stock: '1'
+        },
+        {
+          artnumber: '123459',
+          name: 'Корм 4',
+          brand: 'Felix',
+          weight: '1500',
+          quantity: '30',
+          price: '3450',
+          stock: '0'
+        },
+        {
+          artnumber: '1234510',
+          name: 'Корм 5',
+          brand: 'Sheba',
+          weight: '1500',
+          quantity: '20',
+          price: '20',
+          stock: '0'
+        },
+        {
+          artnumber: '1234511',
+          name: 'Корм 6',
+          brand: 'Sheba',
+          weight: '1500',
+          quantity: '60',
+          price: '50',
+          stock: '1'
+        },
+        {
+          artnumber: '1234512',
+          name: 'Корм 7',
+          brand: 'Royal Canin',
+          weight: '150',
+          quantity: '10',
+          price: '150',
+          stock: '1'
+        },
+        {
+          artnumber: '1234512',
+          name: 'Корм 8',
+          brand: 'Royal Canin',
+          weight: '9000',
+          quantity: '1',
+          price: '6000',
+          stock: '0'
+        },
+        {
+          artnumber: '1234513',
+          name: 'Корм 9',
+          brand: 'Whiskas',
+          weight: '800',
+          quantity: '1',
+          price: '10',
+          stock: '1'
+        },
+        {
+          artnumber: '1234515',
+          name: 'Корм 10',
+          brand: 'Whiskas',
+          weight: '1500',
+          quantity: '1',
+          price: '860',
+          stock: '0'
+        },
+        {
+          artnumber: '1234514',
+          name: 'Корм 11',
+          brand: 'Petcurean',
+          weight: '800',
+          quantity: '20',
+          price: '1150',
+          stock: '1'
+        },
+        {
+          artnumber: '123456654',
+          name: 'Корм 12',
+          brand: 'AlmoNature',
+          weight: '500',
+          quantity: '10',
+          price: '1800',
+          stock: '1'
+        }
+      ],
+      sortedGoods: [],
+      pageSize: 4,
+      currentPage: 0,
+      totalPage: 0
     }
   },
   methods: {
     changeSelect() {
-      this.showUpto = this.pageSize
-      this.currentPage = 1
-      this.showFromto = 0
+      console.log('changed Select')
     },
-    byBrand(){
-      this.sorted= this.goods.filter(elem =>{
-        for(i=0; i<this.checkedBrands.length; i++) {
-          if(elem.brand=== this.checkedBrands[i]) {
-            return elem.brand
-          }
-        }
-      })
+    previousPage() {
+      this.currentPage - 1
+      console.log(' click previos page')
+    },
+    nextPage() {
+      this.currentPage + 1
+      console.log('click next page')
+    }
+  },
+  computed: {
+    sortingGoods() {
+      this.sortedGoods = good
     }
   }
 })
-
